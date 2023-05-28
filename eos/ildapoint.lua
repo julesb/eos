47e3085d1616aec84ead3988cdb5235649301ea6
julesb <julesb@users.noreply.github.com>
2023-05-28 15:03:33 +0930

    fixes

--- eos/ildapoint.lua
+++ eos/ildapoint.lua
@@ -1,7 +1,7 @@
 IldaPoint = {}
 IldaPoint.__index = IldaPoint
 
-function IldaPoint:new(x, y, z, colorIdx, rgb, blank, last)
+function IldaPoint:new(x, y, z, colorIdx, _rgb, blank, last)
     local self = setmetatable({}, IldaPoint)
     self.x = x or 0
     self.y = y or 0
@@ -11,18 +11,21 @@ function IldaPoint:new(x, y, z, colorIdx, rgb, blank, last)
     self.blank = blank or false
     self.last = last or false
 
-    if rgb ~= nil and #rgb == 3 then
-        self.rgb = rgb
+    if self.blank then
+      return self
+    end
+
+    if _rgb ~= nil and #_rgb == 3 then
+        self.rgb = { _rgb[1], _rgb[2], _rgb[3] }
     else
         if colorIdx ~= nil and colorIdx > 0 and colorIdx <= 64 then
-            local rgb = IldaUtil.DEFAULT_PALETTE[colorIdx]
-            if rgb[1] ~= nil and rgb[2] ~= nil and rgb[3] ~= nil then
-                self.rgb[1] = rgb[1] / 255.0
-                self.rgb[2] = rgb[2] / 255.0
-                self.rgb[3] = rgb[3] / 255.0
-            else
-                self.rgb = {1, 1, 1}
-            end
+            local pcol = IldaUtil.DEFAULT_PALETTE[colorIdx]
+            self.rgb[1] = pcol[1] / 255.0
+            self.rgb[2] = pcol[2] / 255.0
+            self.rgb[3] = pcol[3] / 255.0
+        else
+          self.rgb = { 1, 0, 0 }
+          print("colorIdx out of range:", colorIdx)
         end
     end
 
