95542fbcbfd9b24a4828e7418adfe600acd2dda9
julesb <julesb@users.noreply.github.com>
2023-06-14 23:41:50 +0930

    debugging

--- eos/ildafiletest.lua
+++ eos/ildafiletest.lua
@@ -1,58 +1,33 @@
-socket = require("socket")
+local socket = require("socket")
 
-function sleep(sec)
+local function sleep(sec)
     socket.select(nil, nil, sec)
 end
 
 
-function getmemusage()
-    return string.format("used: %dMB", collectgarbage("count") / 1024)
+local function getmemusage()
+    return string.format("used: %sMB", collectgarbage("count") / 1024)
 end
 
-function dotest()
+local function dotest()
 
     local IldaFile = require("ildafile")
-    
+
     print(getmemusage())
 
-    local file = IldaFile:new("unloadable/Moondance.ILD", "SPOOKY")
+    local file = IldaFile:new("../ILDA/YOWZA.ild", "ILDATESTA")
+    --local file = IldaFile:new("../ILDA/Z3.ILD", "ILDATESTA")
+    --local file = IldaFile:new("../ILDA/ildatsta.ild", "ILDATESTA")
     print("LOADED FILE: ", file:toString())
-    print(getmemusage())
 
+    for fidx, frame in ipairs(file.frames) do
+        print(string.format("\tframe[%d/%d]: points: %d", fidx, #file.frames, #frame / 5))
+    end
 
---     print("\t\tDUMP FILE")
---     print(file:toString())
---     for fidx, frame in ipairs(file.frames) do
---         print("\t", frame:toString())
---         --for pidx, point in ipairs(frame.points) do
---         --    print("\t\t", point:toString())
---         --  end
---     end
-
-    print("sleeping...")
-    sleep(10)
-
-    print("releasing file")
---     for _, frame in ipairs(file.frames) do
---         for _, _ in ipairs(frame.points) do
---             frame.points[_] = nil
---         end
---         frame.points = nil
---         file.frames[_] = nil
---     end
---     file.frames = nil
-    file = nil
-    collectgarbage("collect")
-    print("done")
-    print(getmemusage())
 
 end
 
 dotest()
+print("DONE")
 
-while true do
-    print("sleeping...")
-    sleep(5)
-    print(getmemusage())
-end
 
