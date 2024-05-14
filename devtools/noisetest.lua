121d06d0df80a7e55269c77d07b9d657a7bf6a1b
julesb <julesb@users.noreply.github.com>
2024-05-14 23:55:29 +0930

    noise test

--- devtools/noisetest.lua
+++ devtools/noisetest.lua
@@ -1,34 +1,72 @@
 
-local simplex = require("simplex2")
 local osimplex = require("opensimplex2s")
+local S = osimplex.new()
 
 
-local x, y, z, w, t
-local COLUMNS = 129
+local x, y, z, w, t, t2
+local COLUMNS =257
 local CENTER = math.floor(COLUMNS/2)
 x = 0.0
 y = 0.0
 z = 0.0
 w = 0.0
 t = 0.0
+t2 = 0.0
+local off = 0.1
 
-local S = osimplex.new()
+
+local colors = {
+    "\27[31m", -- Red
+    "\27[33m", -- Yellow
+    "\27[32m", -- Green
+    "\27[34m", -- Blue
+    "\27[35m", -- Magenta
+    "\27[36m", -- Cyan
+    "\27[37m", -- White
+}
+local reset = "\27[39m"
+
+local buf = {}
 
 while true do
-  t = t + 0.05
+  -- reset buffer
+  for i = 1, COLUMNS do buf[i] = 0 end
 
-  -- local n = simplex.noise4d(x, y, z, t)
-  local n = S:noise4_Classic(t, y, z, w)
-  local column = math.floor((n + 1) / 2 * (COLUMNS - 0) + 0.5)
-  local output = string.rep(" ", COLUMNS)
+  t = t + 0.005
+  t2 = t2 + 0.006
+
+  local n0 = S:noise4_Classic(x-off, y, t2, t*8)
+  local n1 = S:noise4_Classic(x,     y, t2, t)
+  local n2 = S:noise4_Classic(x+off, y, t2, t/4)
+
+  local column0 = math.floor((n0 + 1) / 2 * (COLUMNS - 0) + 0.5)
+  local column1 = math.floor((n1 + 1) / 2 * (COLUMNS - 0) + 0.5)
+  local column2 = math.floor((n2 + 1) / 2 * (COLUMNS - 0) + 0.5)
+
+  buf[column0] = buf[column0] + 1
+  buf[column1] = buf[column1] + 2
+  buf[column2] = buf[column2] + 4
+
+  local output = "" -- = string.rep(" ", COLUMNS)
   local tstr = string.format("%.3f ", t)
-  output = output:sub(1, column) .. "#" .. output:sub(column + 2)
 
-  if column ~= CENTER then
-    output = output:sub(1, CENTER) .. "." .. output:sub(CENTER + 2)
+  local colidx, color
+  for i=1,COLUMNS do
+    colidx = buf[i]
+    if colidx == 0 then
+      color = colors[6]
+      if i == CENTER then
+        output = output .. "."
+      else
+        output = output .. " "
+      end
+    else
+      color = colors[colidx]
+      output = output .. color .. "#" .. reset
+    end
   end
 
   print(tstr .. "|" .. output .. "|")
-  os.execute("sleep " .. tonumber(0.05))
+  os.execute("sleep " .. tonumber(0.01))
 end
 
