6d6a54374b3dff8e7e19cd02abada8591f29c548
julesb <julesb@users.noreply.github.com>
2025-06-01 22:51:34 +0930

    stretchscan livecode

--- livecode/stretchscan.lua
+++ livecode/stretchscan.lua
@@ -1,7 +1,6 @@
 ---@diagnostic disable: undefined-global
 ---@diagnostic disable: lowercase-global
 
-
 local eos = require("eos")
 local pb = require("pixelbuffer")
 local plugins = require("pixelplugins")
@@ -9,16 +8,15 @@ local plugins = require("pixelplugins")
 local numpoints = 192
 local noise_amp = 0.1
 local noise_divergence = 0.83
-local dwellnum = 2
-local cellwidth = 0.0
 
 local gradcolor1 = {r=1, g=0, b=0}
 local gradcolor2 = {r=0, g=0, b=1}
 local colormode = 0 -- 0 = HSV, 1 = gradient
 
 local timestep = 0.3
-local bufsize = 256
+local bufsize = 1024
 local noisebuf = pb.new(bufsize)
+local stretchbuf = pb.new(bufsize)
 
 function init()
   return true
@@ -34,17 +32,11 @@ end
 
 
 function get_points(time)
-  local v2 = require("vec2")
   local cs = require("colorspace")
-  local out = {}
-  local cellrad = (2.0 / numpoints) * cellwidth / 2.0
-  local edgeoffset = { x = cellrad, y = 0, r = 0, g = 0, b = 0 }
   local drift = time * 0.05
 
-  local stretchpoints = {}
-  local points = {}
-
-  noisebuf:apply_effect(plugins.gen.rgbnoise(0.2, time))
+  noisebuf:apply_effect(plugins.gen.rgbnoise(0.0001, time*timestep*1))
+  stretchbuf:clear()
 
   for i = 1,numpoints do
     local t = (i-1) / (numpoints-1) -- t goes from 0 to 1
@@ -63,46 +55,18 @@ function get_points(time)
       c = cs.hcl_gradient(gradcolor1, gradcolor2, t)
     end
 
-    stretchpoints[i] = eos.newpoint(nx, 0, c.r, c.g, c.b)
+    stretchbuf:set_pixel(nx, c)
   end
 
-  local noisepoints = noisebuf:as_points_optimized()
+  -- noisebuf:blend("screen")(stretchbuf)
+  -- noisebuf:blend("add")(stretchbuf)
+  -- noisebuf:blend("max")(stretchbuf)
+  -- noisebuf:blend("min")(stretchbuf)
+  -- noisebuf:blend("subtract")(stretchbuf)
+  noisebuf:blend("difference")(stretchbuf)
+  -- points = noisebuf:as_points_optimized()
 
-
-    for i=1, #stretchpoints do
-      table.insert(points, stretchpoints[i])
-    end
-    -- for i=1,#noisepoints do
-    --   table.insert(points, noisepoints[i])
-    -- end
-
-  -- sort points  by x position
-  table.sort(points, function(a, b)
-    return a.x < b.x
-  end)
-
-  for i=1, #points do
-    if cellwidth > 0 then
-      local c = {r=points[i].r, g=points[i].g, b=points[i].b}
-      local left = v2.sub(points[i], edgeoffset)
-      -- left.x = eos.wrap_neg1_to_1(left.x)
-      eos.setcolor(left, c)
-      local right = v2.add(points[i], edgeoffset)
-      -- right.x = eos.wrap_neg1_to_1(right.x)
-      eos.setcolor(right, c)
-
-      eos.addblank(out, left, dwellnum)
-      eos.addpoint2(out, left, 1)
-      eos.addpoint2(out, right, dwellnum)
-      eos.addblank(out, right, 1)
-    else
-      eos.addblank(out, points[i], dwellnum)
-      eos.addpoint2(out, points[i], dwellnum)
-      eos.addblank(out, points[i], 1)
-    end
-  end
-
-  return out
+  return eos.points_to_xyrgb(noisebuf:as_points_optimized())
 end
 
 
